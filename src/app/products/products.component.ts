422d2861ebcfc04a5c0fb988dac0fe020be49495
lwairore <kwairore@gmail.com>
2020-10-04 21:57:46 +0300

    - add OnPush as a ChangeDetectionStrategy here since we are using immutability to get all our products. This leads to better performance.

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { ProductService, IProduct } from '../product.service';
 
 @Component({
   selector: 'in-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductsComponent implements OnInit {
+  products$: Observable<IProduct[]> = this.productService.products$;
 
-  constructor() { }
+  constructor(
+    private productService: ProductService,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  trackById(index, item){
+    return item.id;
   }
 
 }
