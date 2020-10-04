b0c7acf54151fc531749c81f63ef8e7b1771d3cc
lwairore <kwairore@gmail.com>
2020-10-04 22:03:17 +0300

    - Generate DeleteProductModalComponent component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProductsComponent } from './products/products.component';
+import { DeleteProductModalComponent } from './delete-product-modal/delete-product-modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent
+    ProductsComponent,
+    DeleteProductModalComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/delete-product-modal/delete-product-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'in-delete-product-modal',
+  templateUrl: './delete-product-modal.component.html',
+  styleUrls: ['./delete-product-modal.component.scss']
+})
+export class DeleteProductModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
