20376c9b3099443c5633d994711fa955499bf098
lwairore <kwairore@gmail.com>
2020-10-05 11:41:58 +0300

    - add the removeProduct method to our ProductsService.

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -114,4 +114,13 @@ export class ProductService {
   products$ = new BehaviorSubject<IProduct[]>(this.products);
 
   constructor() { }
+
+  removeProduct(product) {
+    const index = this.products.indexOf(product);
+    this.products = [
+      ...this.products.slice(0, index),
+      ...this.products.slice(index + 1)
+    ];
+    this.products$.next(this.products);
+  }
 }
