e8269a352a1ff02ee9adf4990c2f799a4fb8c615
lwairore <kwairore@gmail.com>
2020-10-05 13:27:17 +0300

    - use FormGroup to group a set of AbstractControl.

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,13 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'in-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductComponent implements OnInit {
+  productForm: FormGroup;
+  @Input() product;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.productForm = this.fb.group({
+      basic: fb.group({
+        name: '',
+        description: '',
+        active: false,
+        features: fb.array([
+          fb.control('')
+        ])
+      }),
+      expiration: fb.group({
+        expirationDate: null
+      })
+    })
+  }
 
   ngOnInit(): void {
   }
