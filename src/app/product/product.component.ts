3720c0cc43258316a6b49a1d9522e87b0a279459
lwairore <kwairore@gmail.com>
2020-10-05 15:56:23 +0300

    - use EventEmitter to send all the form values that we enter on the three steps as a product. Then, we call close, which resets the form and the wizard, and also takes us to the first step of the wizard on close. On handleClose, we emit nothing in finish and call close.

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, Input, Output, ViewChild, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, FormArray, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
+import { ClrWizard } from '@clr/angular';
 
 function minDateValidation(date: Date): ValidatorFn {
   return (control: AbstractControl): { [key: string]: any } | null => {
@@ -18,70 +19,96 @@ function minDateValidation(date: Date): ValidatorFn {
 export class ProductComponent implements OnInit {
   productForm: FormGroup;
   @Input() product;
+  @Output() finish = new EventEmitter();
+  @ViewChild('productWizard', { static: false }) productWizard: ClrWizard;
+
   deviceType = 'tablet';
 
-  deviceTypes = [
-    {
-      name: 'Tablet',
-      icon: 'tablet'
-    },
-    {
-      name: 'Laptop',
-      icon: 'computer'
-    },
-    {
-      name: 'Phone',
-      icon: 'mobile'
-    },
-    {
-      name: 'Monitor',
-      icon: 'display'
-    }
-  ];
+  deviceTypes = [{
+    name: 'Tablet',
+    icon: 'tablet',
+  }, {
+    name: 'Laptop',
+    icon: 'computer'
+  }, {
+    name: 'Phone',
+    icon: 'mobile'
+  }, {
+    name: 'Monitor',
+    icon: 'display'
+  }];
+
+  selectDevice(device) {
+      this.deviceType = device.icon;
+  }
 
   constructor(private fb: FormBuilder) {
-    this.productForm = this.fb.group({
-      basic: fb.group({
-        name: ['', Validators.required],
-        description: '',
-        active: false,
-        features: fb.array([
-          fb.control('')
-        ])
-      }),
-      expiration: fb.group({
-        expirationDate: [null,
-          Validators.compose([Validators.required, minDateValidation(new Date())])]
-      })
-    });
+      this.productForm = this.fb.group({
+          basic: fb.group({
+              name: ['', Validators.required],
+              description: '',
+              active: false,
+              features: fb.array([
+                  fb.control('')
+              ])
+          }),
+          expiration: fb.group({
+              expirationDate: [null, Validators.compose([Validators.required, minDateValidation(new Date())])],
+          })
+      });
   }
 
   ngOnInit(): void {
-  }
-
-  selectDevice(device) {
-    this.deviceType = device.icon;
   }
 
   get basicFeatures(): FormArray {
     return this.productForm.get('basic.features') as FormArray;
   }
 
-  get expirationError() {
-    if (this.productForm.get('expiration.expirationDate').hasError('required')) {
-      return 'This Field is Required!';
-    }
-    if (this.productForm.get('expiration.expirationDate').hasError('minDateValidation')) {
-      return 'Expiration should be after today\'s date';
-    }
-  }
-
   get isBasicInvalid(): boolean {
     return this.productForm.get('basic').invalid;
-  }
+}
 
   addFeature() {
     this.basicFeatures.push(this.fb.control(''));
   }
+
+  get expirationError() {
+    if (this.productForm.get('expiration.expirationDate').hasError('required')) {
+        return 'This Field is Required!';
+    }
+    if     (this.productForm.get('expiration.expirationDate').hasError('minDateValidation')) {
+        return 'Expiration should be after today\'s date';
+    }
+}
+
+
+
+    get isExpirationInvalid(): boolean {
+        return this.productForm.get('expiration').invalid;
+    }
+
+    handleClose() {
+        this.finish.emit();
+        this.close();
+    }
+
+    close() {
+        this.productForm.reset();
+        this.deviceType = 'tablet';
+        this.productWizard.goTo(this.productWizard.pageCollection.pages.first.id);
+        this.productWizard.reset();
+    }
+
+    handleFinish() {
+        this.finish.emit({
+            product: {
+                type: this.deviceType,
+                ...this.productForm.get('basic').value,
+                ...this.productForm.get('expiration').value,
+            }
+        });
+        this.close();
+    }
 
 }
